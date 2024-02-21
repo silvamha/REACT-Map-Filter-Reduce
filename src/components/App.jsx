b8000952711a9ad41b23f61a6d42fbe79e0bf88f
Ed <silva.email@yahoo.com>
2024-02-21 17:42:05 -0500

    Refactor App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import emojipedia from "../emojipedia";
 
-
-
-
 const App = () => {
-    return (
-        <div>
-            <h1>This is only a test</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>This is only a test</h1>
+    </div>
+  );
+};
 
 <div>
-    <App />
-</div>
-
-
-
+  <App />
+</div>;
 
 export default App;
