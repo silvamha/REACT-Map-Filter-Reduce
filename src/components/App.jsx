807c204d72d770201dd13077c1ccc6261b5be024
Ed <silva.email@yahoo.com>
2024-02-21 09:12:06 -0500

    Add @babel/plugin-proposal-private-property-in-object dependency and create new component App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+
+const populateNewMeaning = (entry) =>{
+    return entry.meaning
+}
+const newMeaning = emojipedia.map()
+
+console.log(emojipedia)
+
+console.log(newMeaning)
+
+
+const App = () =>{
+    return <div>
+        <h1>This is only a test</h1>
+    </div>
+}
+
+<div>
+    <App />
+</div>
+
+
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-var numbers = [3, 56, 2, 48, 5];
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App.jsx"; 
+
+// var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
@@ -9,3 +13,6 @@ var numbers = [3, 56, 2, 48, 5];
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
+
+
+ReactDom.render(<App />, document.getElementById('root'));
