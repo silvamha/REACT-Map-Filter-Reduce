41c2f3efc4bc8331694a4e59b1de304d4b23d835
Ed <silva.email@yahoo.com>
2024-02-22 11:04:26 -0500

    Refactor App component and add Info component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import emojipedia from "../emojipedia";
+import Info from "./Info";
+
+const createNewMeaning = (newMeaning) =>  {
+  return (<Info 
+  key = {newMeaning.id}
+  meaning = {newMeaning.meaning.substring(0, 100)}
+  />)
+
+  
+}
 
 const App = () => {
   return (
     <div>
-          <h1>This is only a test</h1>  
-          <p>Only a test</p>
+      <h1>This is only a test</h1>
+      <p>Only a test</p>
+      {emojipedia.map(createNewMeaning)}
+    
+ 
     </div>
   );
 };
 
 <div>
   <App />
+  <Info />
 </div>;
 
 export default App;
---
+++ src/components/Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Info = (props) => {
+  return ( <p>
+  {props.meaning}
+  </p>);
+};
+
+
+export default Info
